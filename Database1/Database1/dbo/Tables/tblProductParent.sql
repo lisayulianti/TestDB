47a1e3d1bec3953ee89b9bdeb2b7d6ae254abf55
lisayulianti <lisa_yulianti@lexia-analytics.com>
2014-10-14 17:28:02 +0800

    update tblProductParent

--- Database1/Database1/dbo/Tables/tblProductParent.sql
+++ Database1/Database1/dbo/Tables/tblProductParent.sql
@@ -3,6 +3,8 @@
     [productParentID]   INT            NOT NULL,
     [ProductParentCode] NVARCHAR (100) NULL,
     [importLogID]       INT            NULL,
+    [test1] NCHAR(10) NULL, 
+    [test2] NCHAR(10) NULL, 
     CONSTRAINT [PK_tblProductParent] PRIMARY KEY CLUSTERED ([productParentID] ASC)
 );
 
